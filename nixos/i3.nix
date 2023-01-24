49c63968f056b3d0496b115f609c815f7e98af44
dashwav <dashwav@gmail.com>
2023-01-24 04:03:12 -0700

    Initial commit

---
+++ nixos/i3.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, callPackage, ...}:
+
+{
+  environment.pathsToLink = ["/libexec"];
+  services.xserver = {
+    enable = true;
+    desktopManager = {
+      xterm.enable = false;
+    };
+    
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu
+        i3status
+        i3lock
+        i3blocks
+
+      ];
+    };
+  };
+}
