707569ef4367c1397b23f763154c03f0bd0ce566
dashwav <dashwav@gmail.com>
2023-01-28 21:06:04 -0700

    add git config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -58,7 +58,11 @@
   };
   # Enable home-manager and git
   programs.home-manager.enable = true;
-  programs.git.enable = true;
+  programs.git = {
+    enable = true;
+    userName = "dashwav";
+    userEmail = "dashwav@gmail.com";
+  };
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
